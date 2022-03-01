4b6eaff09ecd5a30a5943186ad0f243e4c95d388
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-03-02 02:06:33 +0500

    Final Updated

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+export default class App extends Component {
+  pageSize = 9;
+  apiKey = process.env.REACT_APP_NEW_API;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({
+      progress: progress,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar
+            height={3}
+            color="#f11946"
+            progress={this.state.progress}
+          />
+          <Switch>
+            <Route exact path="/">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="general"
+                pageSize={this.pageSize}
+                category="general"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/business">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="business"
+                pageSize={this.pageSize}
+                category="business"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="entertainment"
+                pageSize={this.pageSize}
+                category="entertainment"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/general">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="general"
+                pageSize={this.pageSize}
+                category="general"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="health"
+                pageSize={this.pageSize}
+                category="health"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/science">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="science"
+                pageSize={this.pageSize}
+                category="science"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="sports"
+                pageSize={this.pageSize}
+                category="sports"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="technology"
+                pageSize={this.pageSize}
+                category="technology"
+                country="in"
+              />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
